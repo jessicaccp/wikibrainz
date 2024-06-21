5384edf5c4866f00bdd3555cc5cbba39248c739e
Jessica Patricio <jessicacacau@gmail.com>
2024-06-21 13:26:10 -0300

    add base of project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [url, setUrl] = useState(
+    `https://musicbrainz.org/ws/2/artist/?query=artist:"the+beatles"&limit=10&offset=0&fmt=json`
+  );
+  const [keyword, setKeyword] = useState("");
+  const [data, setData] = useState([]);
+  const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error(error));
+  }, [url]);
+
+  useEffect(() => {
+    setResult([]);
+    if (data && data.artists)
+      data.artists.forEach((artist, key) =>
+        setResult((prev) => [
+          ...prev,
+          <li key={key}>
+            {artist.name} - {artist.disambiguation}
+          </li>,
+        ])
+      );
+  }, [data]);
+
+  const handleInput = (event) => {
+    setKeyword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    if (event.key === "Enter") {
+      setUrl(
+        `https://musicbrainz.org/ws/2/artist/?query=artist:"${keyword}"&limit=10&offset=0&fmt=json`
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <input
+        type="search"
+        name="search"
+        onChange={handleInput}
+        onSubmit={handleSubmit}
+        onKeyDown={handleSubmit}
+      />
+      <div id="result">{result}</div>
     </div>
   );
 }
-
-export default App;
